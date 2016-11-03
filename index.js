e59c0ebacd52a19d4edb9404914c3d143af7fc16
Alessandro <a.romanino@gmial.com>
2016-11-03 16:02:46 +0100

    read properties bug fixed

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
+var config = require("../.././config/properties.json");
+var async=require('async');
+var argv = require('minimist')(process.argv.slice(2));
+var conf;
+var key;
+switch (process.env['NODE_ENV']) {
+    case 'dev':
+        conf = config.dev || config.production;
+        key= config.dev ? 'dev' : 'production';
+        break;
+    case 'test':
+        key= config.test ? 'test' : 'production';
+        conf = config.test || config.production;
+        break;
+    default:
+        conf = config.production;
+        key='production';
+        break;
+}
+
+async.eachOf(conf, function(param,index,callback) {
+    console.log('Processing Key ' + index);
+    if(argv[index])
+        conf[index]=argv[index];
+    callback();
+},function(err){
+    config[key]=conf;
+});
 
 
 
-
-exports.readProperties=function(path){
-    var config = require(path);
-    var async=require('async');
-    var argv = require('minimist')(process.argv.slice(2));
-
-    switch (process.env['NODE_ENV']) {
-        case 'dev':
-            conf = config.dev || config.production;
-            break;
-        case 'test':
-            conf = config.test || config.production;
-            break;
-        default:
-            conf = config.production;
-            break;
-    }
-
-
-
-    async.each(conf, function(param, callback) {
-        console.log('Processing Key ' + param);
-        if(argv[param])
-            conf[param]=argv[param];
-        callback();
-    });
- return(conf);
-};
+exports.conf=conf;
+exports.conf=config;
